ee45ffde5a0d0b5c0e9c9af9c280bb5194c09574
Kabish <kabishsridar6@gmail.com>
2025-06-26 19:42:08 +0530

    distance measurement

--- Distance Estimation/2. marker_detection/main.py
+++ Distance Estimation/2. marker_detection/main.py
@@ -3,37 +3,43 @@ from cv2 import aruco
 import numpy as np
 
 # dictionary to specify type of the marker
-marker_dict = aruco.getPredefinedDictionary(aruco.DICT_5X5_250)
+marker_dict = aruco.getPredefinedDictionary(aruco.DICT_5X5_250) # gets the dictionary type and store it as marker_dict
 
 # detect the marker
-param_markers = aruco.DetectorParameters()
+param_markers = aruco.DetectorParameters() # detects parameters to be used to detect the marker
 
 # utilizes default camera/webcam driver
 cap = cv.VideoCapture(0)
 
 # iterate through multiple frames, in a live video feed
 while True:
-    ret, frame = cap.read()
-    if not ret:
+    ret, frame = cap.read() # reading the camera
+    if not ret: # if it is not read, return cannot read
+        print("Cannot read")
         break
     # turning the frame to grayscale-only (for efficiency)
-    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray_frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY) # converting to gray scale
+    cv.imshow('gray', gray_frame) # displays the window of frame converted to grayscale
     marker_corners, marker_IDs, reject = aruco.detectMarkers(
         gray_frame, marker_dict, parameters=param_markers
-    )
+    ) # detecting the corners
+    # print(marker_corners)
+    # print(marker_IDs)
+    # print(reject)
     # getting conrners of markers
-    if marker_corners:
-        for ids, corners in zip(marker_IDs, marker_corners):
-            cv.polylines(
+    if marker_corners: # if markers corners were found
+        print("marker")
+        for ids, corners in zip(marker_IDs, marker_corners): # this combines both the id and corners
+            cv.polylines( # draws the outline of the aruco marker
                 frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
-            )
-            corners = corners.reshape(4, 2)
+            ) # astype is used so that it will be converted to integers
+            corners = corners.reshape(4, 2) # assigning corners
             corners = corners.astype(int)
             top_right = corners[0].ravel()
             top_left = corners[1].ravel()
-            bottom_right = corners[2].ravel()
+            bottom_right = corners[2].ravel() # ravel makes the numpy array into single line
             bottom_left = corners[3].ravel()
-            cv.putText(
+            cv.putText( # inserting text of the id 
                 frame,
                 f"id: {ids[0]}",
                 top_right,
@@ -43,10 +49,10 @@ while True:
                 2,
                 cv.LINE_AA,
             )
-            # print(ids, "  ", corners)
-    cv.imshow("frame", frame)
+            print(ids, "  ", corners)
+    cv.imshow("frame", frame) # displays the frame
     key = cv.waitKey(1)
-    if key == ord("q"):
+    if key == ord("q"): # break when user presses q
         break
 cap.release()
 cv.destroyAllWindows()
--- Distance Estimation/3.1 camera_calibration/calibration_script.py
+++ Distance Estimation/3.1 camera_calibration/calibration_script.py
@@ -1,9 +1,9 @@
-import cv2 as cv
+import cv2 as cv # imporing required modules
 import os
 import numpy as np
 
 # Chess/checker board size, dimensions
-CHESS_BOARD_DIM = (9, 6)
+CHESS_BOARD_DIM = (9, 6) # the dimension of chessboard(there should be exact dimension where we caliberate and we capture)
 
 # The size of squares in the checker board design.
 SQUARE_SIZE = 22  # millimeters (change it according to printed size)
@@ -11,14 +11,12 @@ SQUARE_SIZE = 22  # millimeters (change it according to printed size)
 # termination criteria
 criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-
 calib_data_path = "../calib_data"
 CHECK_DIR = os.path.isdir(calib_data_path)
 
-
 # saving the image / camera calibration data
 
-if not CHECK_DIR:
+if not CHECK_DIR: # checks whether the Dir is presented
     os.makedirs(calib_data_path)
     print(f'"{calib_data_path}" Directory is created')
 
@@ -26,30 +24,31 @@ else:
     print(f'"{calib_data_path}" Directory already Exists.')
 
 # prepare object points, i.e. (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+# will be np.zeros(9 * 6, 3) and converted to float will return 0s of 54 rows and 3 columns
 obj_3D = np.zeros((CHESS_BOARD_DIM[0] * CHESS_BOARD_DIM[1], 3), np.float32)
 
 obj_3D[:, :2] = np.mgrid[0 : CHESS_BOARD_DIM[0], 0 : CHESS_BOARD_DIM[1]].T.reshape(
     -1, 2
 )
 obj_3D *= SQUARE_SIZE
-print(obj_3D)
+print(obj_3D) # returns the value of the matrix
 
 # Arrays to store object points and image points from all the given images.
 obj_points_3D = []  # 3d point in real world space
 img_points_2D = []  # 2d points in image plane
 
 # The images directory path
-image_dir_path = "images"
+image_dir_path = "C:\\57_challenges\\OpenCV_silicon_jelly\\Distance Estimation\\3.1 camera_calibration\\images"
 
 files = os.listdir(image_dir_path)  # list of names of all the files present
 for file in files:
-    print(file)
+    print(file) # displays the file names in the folder
     imagePath = os.path.join(image_dir_path, file)
     # print(imagePath)
 
-    image = cv.imread(imagePath)
-    grayScale = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    ret, corners = cv.findChessboardCorners(image, CHESS_BOARD_DIM, None)
+    image = cv.imread(imagePath) # reads the image
+    grayScale = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # converting to grayscale
+    ret, corners = cv.findChessboardCorners(image, CHESS_BOARD_DIM, None) # detects the chess board corners
     if ret == True:
         obj_points_3D.append(obj_3D)
         corners2 = cv.cornerSubPix(grayScale, corners, (3, 3), (-1, -1), criteria)
@@ -61,7 +60,7 @@ cv.destroyAllWindows()
 # h, w = image.shape[:2]
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(
     obj_points_3D, img_points_2D, grayScale.shape[::-1], None, None
-)
+) # caliberation part
 print("calibrated")
 
 print("dumping the data into one files using numpy ")
@@ -71,7 +70,7 @@ np.savez(
     distCoef=dist,
     rVector=rvecs,
     tVector=tvecs,
-)
+) # creating a file and storing all the datas to the file named MultiMatrix
 
 print("-------------------------------------------")
 
