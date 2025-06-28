8618499c4bf6559cbe801941fcd12b8e16179d94
kabishsridar <kabishsridar@6gmail.com>
2025-06-28 09:32:02 +0530

    traced x and y axis

--- Distance_Estimation/2. marker_detection/rpi_detection.py
+++ Distance_Estimation/2. marker_detection/rpi_detection.py
@@ -19,6 +19,8 @@ picam2.configure(config)
 picam2.start()
 time.sleep(0.01)
 cv.namedWindow("RPI", cv.WINDOW_NORMAL)
+x = []
+y = []
 # iterate through multiple frames, in a live video feed
 while True:
     frame = picam2.capture_array("main")
@@ -30,12 +32,16 @@ while True:
     marker_corners, marker_IDs, reject = aruco.detectMarkers(
         gray_frame, marker_dict, parameters=param_markers
     ) # detecting the corners
-    print(marker_corners)
+    if marker_corners:
+        # print(marker_corners[0][0][0][0])
+        x.append(marker_corners[0][0][0][0])
+        y.append(marker_corners[0][0][0][1])
+
     # print(marker_IDs)
     # print(reject)
     # getting conrners of markers
     if marker_IDs is not None and marker_corners is not None: # if markers corners were found
-        print("marker")
+        # print("marker")
         for ids, corners in zip(marker_IDs, marker_corners): # this combines both the id and corners
             cv.polylines( # draws the outline of the aruco marker
                 frame, [corners.astype(np.int32)], True, (0, 255, 255), 4, cv.LINE_AA
@@ -56,10 +62,13 @@ while True:
                 2,
                 cv.LINE_AA,
             )
-            print(ids, "  ", corners)
+
     cv.imshow("frame", frame) # displays the frame
     key = cv.waitKey(1)
     if key == ord("q"): # break when user presses q
         break
+
+for i in range(len(x)):
+    print(f"x: {x[i]}, y: {y[i]}")
 picam2.stop()
 cv.destroyAllWindows()
